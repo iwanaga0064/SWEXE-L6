68e41cf76a1d9f21ea2d5e3ceed1174d07b16ffd
Tomoki Iwanaga <1811140064f@ed.fuk.kindai.ac.jp>
2020-10-27 14:17:58 +0000

    演習6

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    pass = BCrypt::Password.create(params[:user][:pass])
+    @user = User.new(uid: params[:user][:uid], pass: pass)
+    # @user = User.new(uid: params[:user][:uid], pass: params[:user][:pass])
+    if @user.save
+      flash[:info] = "ユーザー登録しました"
+      redirect_to users_path
+    else
+      @user.pass = ""
+      render :new
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:info] = "ユーザーを削除しました"
+    redirect_to users_path
+  end
+end
